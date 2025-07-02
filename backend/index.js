71084bffef76620dfab0da522d243fe026d28f88
siddhi-manikpure <manikpuresiddhi@gmail.com>
2025-07-02 23:27:26 +0530

    1:53:03

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,23 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDb from './config/db.js';
-import authRouter from './routes/auth.routes.js';
-import cookieParser from 'cookie-parser';
-
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
+import connectDb from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 const port = process.env.PORT || 5000;
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth",authRouter)
-
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   connectDb();
---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,18 @@
+import React, { createContext } from "react";
+export const userDataContext = createContext();
+
+function UserContext({ children }) {
+  const serverUrl = "http://localhost:8000";
+  const value = {
+    serverUrl,
+  };
+  return (
+    <div>
+      <userDataContext.Provider value={value}>
+        {children}
+      </userDataContext.Provider>
+    </div>
+  );
+}
+
+export default UserContext;
