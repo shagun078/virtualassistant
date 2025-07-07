93714fd8eb603b892b719e3995e9fa0248e27c30
shagun078 <134071973+shagun078@users.noreply.github.com>
2025-07-07 11:12:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtualassistantt.onrender.com",
     credentials: true,
   })
 );
